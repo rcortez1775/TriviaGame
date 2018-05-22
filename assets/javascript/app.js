c286962132d27ff604dce0ce8dbe6241257c410d
Robert Cortez <Nuhuhfoo@gmail.com>
2018-05-22 08:43:15 -0700

    homework javascript #2

---
+++ assets/javascript/app.js
@@ -0,0 +1,132 @@
+var panel = $("#questionForm");
+
+var questions = [{
+    question: "How long does Zenyatta's Transcendence ultimate ability last?",
+    answers: ["7 seconds", "9 seconds", "6 seconds", "8 seconds"],
+    correctAnswer: "6 seconds"
+  }, {
+    question: "Which Nordic country does Torbjörn hail from??",
+    answers: ["Sweden", "Finland", "Norway", "Iceland"],
+    correctAnswer: "Sweden"
+  }, {
+    question: "What is the name of Bastion's bird?",
+    answers: ["Troy", "Aristotle", "Galileo", "Ganymede"],
+    correctAnswer: "Ganymede"
+  }, {
+    question: "What is the name of the secret organization that Gabriel Reyes lead?",
+    answers: ["Roguewatch", "Darkwatch", "Blackwatch", "Omnicwatch"],
+    correctAnswer: "Blackwatch"
+  }, {
+    question: "What is the name of the Armor-wearing Gorilla?",
+    answers: ["Lucio", "Winston", "Soldier: 76", "Bastion"],
+    correctAnswer: "Winston"
+  }];
+
+  var timer;
+
+var game = {
+
+  correct: 0,
+  incorrect: 0,
+  counter: 30,
+
+  countdown: function() {
+    game.counter--;
+    $("#counter-number").html(game.counter);
+    if (game.counter === 0) {
+      console.log("TIME UP");
+      game.done();
+    }
+  },
+  start: function() {
+    timer = setInterval(game.countdown, 1000);
+
+    $("#body").prepend("<h2>Time Remaining: <span id='counter-number'>30</span> Seconds</h2>");
+
+    $("#start").remove();
+
+    for (var i = 0; i < questions.length; i++) {
+      panel.append("<h2>" + questions[i].question + "</h2>");
+      for (var j = 0; j < questions[i].answers.length; j++) {
+        panel.append("<input type='radio' name='question-" + i +
+        "' value='" + questions[i].answers[j] + "''>" + questions[i].answers[j]);
+      }
+    }
+
+    panel.append("<button id='done'>Done</button>");
+  },
+
+  done: function() {
+
+    $.each($("input[name='question-0']:checked"), function() {
+      if ($(this).val() === questions[0].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-1']:checked"), function() {
+      if ($(this).val() === questions[1].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-2']:checked"), function() {
+      if ($(this).val() === questions[2].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-3']:checked"), function() {
+      if ($(this).val() === questions[3].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-4']:checked"), function() {
+      if ($(this).val() === questions[4].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    this.result();
+
+  },
+
+  result: function() {
+
+    clearInterval(timer);
+
+    $("#body").remove();
+
+    panel.html("<h2>All Done!</h2>");
+    panel.append("<h3>Correct Answers: " + this.correct + "</h3>");
+    panel.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+    panel.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+  }
+};
+
+// CLICK EVENTS
+
+$(document).on("click", "#start", function() {
+  game.start();
+});
+
+
+$(document).on("click", "#done", function() {
+  game.done();
+});
